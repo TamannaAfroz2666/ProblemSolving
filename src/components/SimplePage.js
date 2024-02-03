0d5929e7a2daf8570d45469da8195a7721cf3f75
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-02-03 19:18:51 +0600

    basic form updated

--- src/components/SimplePage.js
+++ src/components/SimplePage.js
@@ -4,26 +4,30 @@ import './SimplePage.css';
 const SimplePage = () => {
     return (
         <div>
-           <div className='formContainer'>
-            <div className="formContainerMain">
-            <form action="">
-                <div className="nameCon">
-                <input type="text" name='' placeholder='name' className='nameField' id='nameField' />
-                </div>
-              <div className="ageCon">
-              <input type="text" name='' placeholder='age' className='nameField' id='nameField' />
+            <div className='formContainer'>
+                <div className="formContainerMain">
+                    <form action="">
+                        <div className="nameCon">
+                            <input type="text" name='' placeholder='name' className='nameField' id='nameField' />
+                        </div>
+                        <div className="ageCon">
+                            <input type="text" name='' placeholder='age' className='nameField' id='nameField' />
 
-              </div>
-               <div className="homeCon">
-               <input type="text" name='' placeholder='home' className='nameField' id='nameField' />
-               </div>
-                <button>submit</button>
-            </form>
+                        </div>
+                        <div className="homeCon">
+                            <input type="text" name='' placeholder='home' className='nameField' id='nameField' />
+                        </div> 
+                        <div className="btnCon">
+                        <button>submit</button>
+
+                        </div>
+                        
+                    </form>
+
+                </div>
+
 
             </div>
-          
-
-           </div>
         </div>
     );
 };
