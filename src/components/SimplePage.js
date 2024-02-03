7b62ce96d0e7fc80519b610fed2027b7b38c8667
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-02-03 18:35:57 +0600

    file create

---
+++ src/components/SimplePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SimplePage.css';
+
+const SimplePage = () => {
+    return (
+        <div>
+            <h1>helo</h1>
+            
+        </div>
+    );
+};
+
+export default SimplePage;
